812a5046e34c2467bc2b514c4719844e0a330e30
unknown <code.afridi@gmail.com>
2025-12-30 18:41:44 +0530

    added auth

--- server/supabase.js
+++ server/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.REACT_APP_SUPABASE_URL,
-  process.env.REACT_APP_SUPABASE_ANON_KEY,
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY,
   {
     auth: {
       persistSession: true,
