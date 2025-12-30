7ba89cd4dcac49d96464bba73eddf97621e56d58
unknown <code.afridi@gmail.com>
2025-12-30 19:12:48 +0530

    added auth

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import LandingPage from "./components/LandingPage";
 import Auth from "./pages/Auth";
@@ -7,7 +7,7 @@ import Protected from "./components/Protected";
 
 export default function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<Auth />} />
