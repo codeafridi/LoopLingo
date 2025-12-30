51804c73001b66c9097402e2c3de1b6b1f190859
unknown <code.afridi@gmail.com>
2025-12-30 18:45:44 +0530

    added auth

--- server/supabase.js
+++ server/supabase.js
@@ -1,13 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+const { createClient } = require("@supabase/supabase-js");
 
-export const supabase = createClient(
+const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY,
-  {
-    auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-      detectSessionInUrl: true, // 🔴 THIS IS THE KEY
-    },
-  }
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
+
+module.exports = { supabase };
