3fca29de0e8bbc364f637623aeba81dadf4803da
unknown <code.afridi@gmail.com>
2026-02-03 19:33:36 +0530

    added oauth

--- server/middleware/temp_supabase.js
+++ server/middleware/temp_supabase.js
@@ -1,4 +1,4 @@
-const supabase = require("../supabase");
+const { supabase } = require("../supabase");
 
 module.exports = async (req, res, next) => {
   const authHeader = req.headers.authorization;
